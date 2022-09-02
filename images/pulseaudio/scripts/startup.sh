f895595fc3ab87c40f5e3d56b305fb14ea3986c5
Sparticuz <kyle@kmcnally.net>
2022-09-02 13:51:38 -0400

    Changing pulseaudio to run as a specified user instead of root, also using some pulseaudio settings from x11docker

--- images/pulseaudio/scripts/startup.sh
+++ images/pulseaudio/scripts/startup.sh
@@ -5,8 +5,8 @@ function LOG {
     echo $(date -R): $0: $*
 }
 
-mkdir -p /home/retro/.config/pulse
-rm -f /home/retro/.config/pulse/*
+mkdir -p $HOME/.config/pulse
+rm -f $HOME/.config/pulse/*
 
 LOG "Starting pulseaudio"
 pulseaudio --log-level=1 #--log-target=stderr -v
